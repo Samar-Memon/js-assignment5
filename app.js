5f38f02fdd60e55d11a542f46599bc8d94503102
Samar-Memon <samar56786raza@gmail.com>
2024-04-01 05:02:22 +0500

    Js-Assgnment-05

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-// for(let i = 1; i <= 10; i++){
-//     document.write(i + '<br>')
-// }
+for(let i = 1; i <= 10; i++){
+    document.write(i + '<br>')
+}
 
-// let userInput = +prompt('Enter a Number for Table');
-// let userLength = +prompt('Table stop Number')
-// for(let j = 1; j <= userLength; j++){
-//     document.write(userInput + ' X  ' + j + ' = ' + userInput*j + '<br>' + '<br>');
-// };
-
-
-// let fruitNames = ['Apple', 'Banana', 'Grapes', 'Papaya', 'Kiwi', 'PineApple'];
-
-// for(let k = 0; k <= fruitNames.length; k++){
-//     document.write('Element at Index of ' +  k + ' ' + fruitNames[k] + '<br>' + '<br>')
-// } 
+let userInput = +prompt('Enter a Number for Table');
+let userLength = +prompt('Table stop Number')
+for(let j = 1; j <= userLength; j++){
+    document.write(userInput + ' X  ' + j + ' = ' + userInput*j + '<br>' + '<br>');
+};
 
 
-// for(let l = 1; l <= 15; l++){
-//     document.write(l + '<br>')
-// }
+let fruitNames = ['Apple', 'Banana', 'Grapes', 'Papaya', 'Kiwi', 'PineApple'];
 
-// for(let m = 15; m >= 1 ;m--){
-//     document.write(m + '<br>')
-// }
-
-// for(let n = 0; n <= 20 ; n += 2){
-//     document.write(n + '<br>')
-// }
-
-// for(let o = 1; o <= 21 ; o += 2){
-//     document.write(o + '<br>')
-// }
-
-// for(let p = 10; p <= 100 ; p += 10){
-    // document.write(p + 'k' + '<br>')
-// }
+for(let k = 0; k <= fruitNames.length; k++){
+    document.write('Element at Index of ' +  k + ' ' + fruitNames[k] + '<br>' + '<br>')
+} 
 
 
-// let userArrayInput = prompt("Enter Any food");
-// let vegetableNames = ['Tomato', 'Potato', 'Carrot', 'Chilli', 'LadyFinger'];
-// let match = false;
-// for(let q = 0; q <= userArrayInput.length; q++){
-//     let upper = userArrayInput.slice(0,1).toUpperCase();
-//     let lower =userArrayInput.slice(1).toLowerCase();
-//     let con = upper + lower;
-//     if(con === vegetableNames[q]){
-//         match = true;
-//         alert(con + ' is Found')
-//     }
-// };
+for(let l = 1; l <= 15; l++){
+    document.write(l + '<br>')
+}
 
-// if(match == false){
-//     alert('Not Found')
-// }
+for(let m = 15; m >= 1 ;m--){
+    document.write(m + '<br>')
+}
+
+for(let n = 0; n <= 20 ; n += 2){
+    document.write(n + '<br>')
+}
+
+for(let o = 1; o <= 21 ; o += 2){
+    document.write(o + '<br>')
+}
+
+for(let p = 10; p <= 100 ; p += 10){
+    document.write(p + 'k' + '<br>')
+}
+
+
+let userArrayInput = prompt("Enter Any food");
+let vegetableNames = ['Tomato', 'Potato', 'Carrot', 'Chilli', 'LadyFinger'];
+let match = false;
+for(let q = 0; q <= userArrayInput.length; q++){
+    let upper = userArrayInput.slice(0,1).toUpperCase();
+    let lower =userArrayInput.slice(1).toLowerCase();
+    let con = upper + lower;
+    if(con === vegetableNames[q]){
+        match = true;
+        alert(con + ' is Found')
+    }
+};
+
+if(match == false){
+    alert('Not Found')
+}
